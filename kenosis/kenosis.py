aa66567fbb339539c6e7c2abcc5a1ba3df6b4f16
VermilionEagle <61714989+VermilionEagle@users.noreply.github.com>
2020-03-03 18:20:31 +1100

    ok boomer

--- kenosis/kenosis.py
+++ kenosis/kenosis.py
@@ -17,7 +17,6 @@ class intro:
     #Defines the lines for looking at the sign.
     def sign():
            print('You read the Neon Sign. The sign reads: "Tutorial".')
-           print('Nate has a fat belly')
            time.sleep(1)
     #Defines the lines for looking at all (in the tutorial).            
     def lookall():
@@ -51,3 +50,47 @@ class intro:
 choice=input("Now you know how to look at things in the world. To list all that you can see, type 'look all'.")"""
         
 
+class Store:
+    #Defines the lines for using the stick
+    def usestick():
+        print("You try to pry at the bars with the stick, but the stick is too thin and breaks before any bars are bent")
+        if "stick" in choice:
+            time.sleep(2)
+            store.usestick()
+    #Defines the lines for using the spoon
+    def usespoon():
+        print("You dig a big enough hole underneath the base of the cage to crawl out the bottom")
+        if "spoon" in choice:
+            time.sleep(3)
+            store.usespoon()
+    #Defines and stores the lines for using the crucifix
+    def usecrucifix():
+         print("The crucifix might be useful later, let's not use it here.")
+         if "crucifix" in choice:
+             store.usecrucifix()
+
+
+class Intro:
+    def intro():
+        #plain text for an introduction to the scene
+        print("You wake on the cold dirt, gasping for air, with the only one you muster leaving a thick layer of dust in your mouth.")
+        print("It`s freezing cold, the only noise you hear are distant crickets and the trees swaying side-to-side as they are caught by the wind")
+        print("You feel around for your glasses, with no luck, but you do find a what feels to be a stick, a spoon, and a crucifix on the ground")
+        time.sleep(2)
+        print("Crucifix Acquired!")
+        print("Upon walking around, you discover that you're locked in a small cage, possibly for chickens. Some of the bars are bent, but you're not strong enough to break them")
+  
+class Game:
+    def challenge(): 
+        #First user challenge
+        print("Here are your options:")
+        time.sleep(2)
+        #Option A
+        print("Will you: Try to pry the cage open with the stick")
+        #Option B
+        print("Will you: Dig a hole underneath the cage with the spoon")
+        #Option C
+        print("Will you: Use the crucifix to pry the cage open")
+        #Asks user for choice
+        choice = input("What will you choose? ")
+    
